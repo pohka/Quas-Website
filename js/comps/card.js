162d2e66f9b27bc5d57d7501fbb07a3cd0cdd9ba
geff <zomg21@hotmail.com>
2018-02-09 13:49:13 +0000

    added cards

---
+++ js/comps/card.js
@@ -0,0 +1,17 @@
+class Card{
+  constructor(img, title, text){
+    this.img = img;
+    this.title = title;
+    this.text = text;
+  }
+
+  render(){
+    <quas>
+      <div class="card">
+        <img src=this.img>
+        <h3>{this.title}</h3>
+        <span>{this.text}</span>
+      </div>
+    </quas>
+  }
+}
--- js/quas-site.js
+++ js/quas-site.js
@@ -3,6 +3,15 @@ function start(){
     {text : "Docs", action:"/docs"},
     {text : "Examples", action:"/"},
   ]);
-  Quas.render(nav, "body");
+  Quas.renderRule(nav, "body", "prepend");
+  let card1 = new Card("/img/code.png", "Super Fast", "Some text");
+  Quas.render(card1, "#card-row-1");
+  let card2 = new Card("/img/code.png", "Easy to learn", "keepo");
+  Quas.render(card2, "#card-row-1");
+  let card3 = new Card("/img/code.png", "Totally Original", "Definately not the poor mans versin of react");
+  Quas.render(card3, "#card-row-1");
+  let card4 = new Card("/img/code.png", "I dont even know", "what to put here");
+  Quas.render(card4, "#card-row-2");
+  Quas.render(card4, "#card-row-2");
   //Quas.remove(nav);
 }
