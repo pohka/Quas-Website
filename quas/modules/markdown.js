fcf32ac7fcfad4504d00b03ae6aad0a21e217011
geff <zomg21@hotmail.com>
2018-07-09 05:23:06 +0100

    added basic structure for markdown rules but not implemented

--- quas/modules/markdown.js
+++ quas/modules/markdown.js
@@ -22,6 +22,115 @@ todo:
   ---
 */
 export({
+
+  init : () => {
+    Markdown.rules = {}
+
+    //need rule for matching paragraphs
+
+    //
+    Markdown.rules["heading"] = {
+      type : "starts-with",
+      pattern : /#+\s+/,
+
+      output : (pattern, line) => {
+        let headingSize = line.match(/#+/)[0].length
+        let match = line.match(pattern)[0];
+        let text = line.substr(text.length);
+
+        return ["h"+headingSize, {}, [text], []];
+      }
+    }
+
+    Markdown.rules["code"] = {
+      type : "block",
+      pattern : /```+/,
+
+      //text == text between open and close block
+      output : (pattern, text) => {
+        if(Quas.hasModule("CodeHighlighter")){
+          return (
+            #<pre>
+              <code q-code="text" data-type="{codeLang}"></code>
+            </pre>
+          );
+        }
+        else{
+          return (
+            #<pre>
+              <code data-type="{codeLang}">{text}</code>
+            </pre>
+          );
+        }
+      }
+    }
+
+    Markdown.rules["ul"] = {
+      type : "starts-with-multiline",
+      pattern : /\*\s+|\s+\*\s+/,
+      output : (pattern, lines) => {
+      //  let initalSpace = parseInt(lines[0].indexOf("*")/2);
+        let parsedLines = [];
+        for(let i=0; i<lines.length; i++){
+          let text = lines[i].substr(lines[i].indexOf("*") + 1)
+          parsedLines.push(text);
+        }
+
+        return #<ul q-for-li="parsedLines"></ul>
+      }
+    }
+
+    Markdown.rules["quote"] = {
+      type : "starts-with-multiline",
+      pattern : />\s+/,
+      output : (pattern, lines) => {
+        for(let i=0; i<lines.length; i++){
+          let match = lines[i].match(pattern);
+            lines[i] = lines[i].substr(match[0].length);
+        }
+        let text = lines.join(" ");
+
+        return #<quote>{text}</quote>
+      }
+    }
+
+    //should just be handling the parsing of the matched text and returning the result node
+    //the text before and after should be solved by a bigger function
+    Markdown.rules["link"] = {
+      type : "inline",
+      pattern : /\[.*?\]\(.*?\)/,
+      output : (pattern, line) => {
+        //let regex = new RegExp(pattern, "g");
+        //let matches =
+        let nodes = [];
+        let hasMatch = true;
+        let text = line;
+        while(hasMatch){
+          let match = text.match(pattern);
+          if(!match){
+            hasMatch = false;
+          }
+          else{
+            let textBefore = text.substr(0, match.index);
+            let matchedText = match[0];
+            text = text.substr(match.index + matchedText.length); //text after
+
+            if(textBefore.length > 0){
+              nodes.push(textBefore);
+            }
+
+            let arr = matchedText.substr(1,matchedText.length-2).split("](");
+
+            //should have case for opening cross origin links with _blank
+            //also using target="push"
+            nodes.push(
+              #<a href="{arr[1]}" target="push">{arr[0]}</a>
+            );
+          }
+        }
+      }
+    }
+  },
   /**
     ---
     Parses markdown text and returns a virtual dom
